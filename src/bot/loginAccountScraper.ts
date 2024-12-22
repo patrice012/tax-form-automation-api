e5c0e998e61066ee6bfa37b3700e0b6eeec0175c
patrice012 <patricedev02@gmail.com>
2024-12-22 18:39:24 +0000

    fix: loading timeout of login page

--- src/bot/loginAccountScraper.ts
+++ src/bot/loginAccountScraper.ts
@@ -142,23 +142,23 @@ export const handleOTPInput = async ({
 };
 
 // Main login function using the helpers
-export const loadPage = async (): Promise<{
-  page: Page;
-  playwrightService: PlaywrightService;
-}> => {
+export const loadPage = async (): Promise<any> => {
   const targetUrl =
     "https://accounts.intuit.com/app/sign-in?app_group=ProConnectTaxOnline&asset_alias=Intuit.apd.ito&redirect_uri=https%3A%2F%2Fito.intuit.com%2Fapp%2Fprotax%2Fsign-in&single_sign_on=false&appfabric=true";
 
   const playwrightService = new PlaywrightService();
+
+  const page = await playwrightService.createPage();
   try {
-    const page = await playwrightService.createPage();
-    await page!.goto(targetUrl, { waitUntil: "networkidle" });
+    await page!.goto(targetUrl, { waitUntil: "networkidle", timeout: 10000 });
     playwrightService.setCurrentPage(page);
     return { page, playwrightService };
   } catch (error) {
     logger.error("Scraping error:", error);
-    throw error;
+    return {page}
   }
+
+ 
 };
 
 export async function loginAccountScraper({ email, password }: IAccount) {
