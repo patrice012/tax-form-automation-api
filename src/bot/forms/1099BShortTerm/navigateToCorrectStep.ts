f0400397ec73096dab9e2905c44a0ee3d7bf9fab
patrice012 <patricedev02@gmail.com>
2025-01-09 19:57:46 +0000

    Fix: reload page after filling 1099BShort and LongTerm

--- src/bot/forms/1099BShortTerm/navigateToCorrectStep.ts
+++ src/bot/forms/1099BShortTerm/navigateToCorrectStep.ts
@@ -7,21 +7,28 @@ import { navigateToForm } from "../../taxFormsFiller/navigateToForm";
 export async function navigateToCorrectStep({ page }: { page: Page }) {
   try {
     let check = await checkElementVisibility({ page });
-    let count = 0;
-    while (check.onDetailPage) {
-      count = count + 1;
+    // let count = 0;
+    // while (check.onDetailPage) {
+    //   count = count + 1;
 
-      if (count > 2) {
-        logger.info("Max click reached, relaoding page");
-        await page.reload();
-        await navigateToForm({ page, linkText: "Dispositions (Sch D, etc.)" });
-        logger.info("Reopen Dispositions (Sch D, etc.) form");
-        break;
-      }
-      logger.info("On detail page, go back to Entry page");
-      await navigateToStep({ page, stepTitle: "Back to Quick entry" });
-      logger.info("Go back to Entry page: success");
-      check = await checkElementVisibility({ page });
+    //   if (count > 2) {
+    //     logger.info("Max click reached, relaoding page");
+    //     await page.reload();
+    //     await navigateToForm({ page, linkText: "Dispositions (Sch D, etc.)" });
+    //     logger.info("Reopen Dispositions (Sch D, etc.) form");
+    //     break;
+    //   }
+    //   logger.info("On detail page, go back to Entry page");
+    //   await navigateToStep({ page, stepTitle: "Back to Quick entry" });
+    //   logger.info("Go back to Entry page: success");
+    //   check = await checkElementVisibility({ page });
+    // }
+
+    if (check.onDetailPage) {
+      logger.info("Relaoding page");
+      await page.reload();
+      await navigateToForm({ page, linkText: "Dispositions (Sch D, etc.)" });
+      logger.info("Reopen Dispositions (Sch D, etc.) form");
     }
 
     if (check.onEntryPage) {
